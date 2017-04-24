ba47ee99b42049a7390a4ace104bd0b245e7b4f1
Chenhao Niu <niu296156@me.com>
2017-04-25 00:11:56 +0800

    Add library for Darwin x64
    Signed-off-by: Chenhao Niu <niu296156@me.com>

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@ namespace sql = sqlpp::sqlite3;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
     logging::Logger::instance().setLogDir(QCoreApplication::applicationDirPath().toStdString());
     logging::Logger::instance().setLogFilePrefix(APP_NAME.toStdString());
 #ifdef _DEBUG
