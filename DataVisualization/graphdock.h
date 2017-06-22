68b8bc9aaa6f2306564f527b5c0b53422fd7893e
Harry Chen <i@harrychen.xyz>
2017-06-22 15:26:30 +0800

    Revert "delete DataVisualization"
    This reverts commit 01c43805302c6e2b360dd84086ab3991f225e04b.

---
+++ DataVisualization/graphdock.h
@@ -0,0 +1,26 @@
+﻿#ifndef GRAPHDOCK_H
+#define GRAPHDOCK_H
+
+#include <QDockWidget>
+#include "qcustomplot.h"
+#include "plot/plotter/plotter.h"
+
+namespace Ui {
+class GraphDock;
+}
+
+class GraphDock : public QDockWidget
+{
+    Q_OBJECT
+
+public:
+    explicit GraphDock(Plotter * ,QWidget *parent = 0);
+    ~GraphDock();
+
+private:
+    Ui::GraphDock *ui;
+    Plotter * pPlotter;
+    void setupMyDemo(QCustomPlot *customPlot){pPlotter -> plot(customPlot);}
+};
+
+#endif // GRAPHDOCK_H
