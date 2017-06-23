f1f05b2253aed2cfc7d36c89e3162f57f903b421
nzp <skynnznk12@163.com>
2017-06-23 20:11:48 +0800

    added comments and fixed a sync bug

--- ui/billlist.cpp
+++ ui/billlist.cpp
@@ -1,10 +1,35 @@
-﻿#include "billlist.h"
+﻿/**
+ * Copyright 2017 OOP-Cash Team
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file   billlist.cpp
+ * @author Zeping Niu <nnznk12@gmail.com>
+ * @date   2017.05
+ * @brief  Implementation file of class BillList
+ */
+
+
+#include "billlist.h"
 
 BillList::BillList(QWidget *parent) : QListWidget(parent)
 {
 	isAdding = false;
 	allFinished = false;
 	_scrollBar = this->verticalScrollBar();
+	/* when the scroll bar moves, the slot will decide whether to emit signal to load more bills */
 	connect(_scrollBar, SIGNAL(valueChanged(int)), this, SLOT(on_scroll_bar_value_changed(int)));
 }
 
@@ -20,6 +45,7 @@ void BillList::setFinished(bool finished)
 
 void BillList::on_scroll_bar_value_changed(int value)
 {
+	/* the bill list need adding iff the scroll bar rolls to the buttom */
 	if(!allFinished && !isAdding && value == _scrollBar->maximum()) {
 		isAdding = true;
 		emit needAdding();
--- ui/billlist.h
+++ ui/billlist.h
@@ -1,30 +1,78 @@
-﻿#ifndef BILLLIST_H
+﻿/**
+ * Copyright 2017 OOP-Cash Team
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+  * @file billlist.h
+  * @author Zeping Niu <nnznk12@gmail.com>
+  * @date 2017.5
+  * @brief Header file for class BillList
+  */
+
+
+#ifndef BILLLIST_H
 #define BILLLIST_H
 
 #include <QListWidget>
 #include <QScrollBar>
 #include <QDebug>
 
+/**
+ * @brief The BillList class
+ */
 class BillList : public QListWidget
 {
 	Q_OBJECT
 public:
+	/**
+	 * @brief BillList
+	 * @param parent
+	 */
 	explicit BillList(QWidget *parent = 0);
 
 public slots:
+	/**
+	 * @brief setIsAddingFalse
+	 * @details set the flag isAdding. when isAdding, truethe BillList will not emit signal.
+	 */
 	void setIsAddingFalse();
+	/**
+	 * @brief setFinished
+	 * @param finished set the flag allFinished
+	 * @details when allFinished the BillList will not emit signal.
+	 */
 	void setFinished(bool finished);
 
 signals:
+	/**
+	 * @brief needAdding
+	 * @details the signal is emitted iff the BillList needs to be filled.
+	 */
 	void needAdding();
 
 private slots:
+	/**
+	 * @brief on_scroll_bar_value_changed
+	 * @param value the scroll bar value
+	 */
 	void on_scroll_bar_value_changed(int value);
 
 private:
-	QScrollBar* _scrollBar;
-	bool isAdding;
-	bool allFinished;
+	QScrollBar* _scrollBar; ///< the scroll bar of the BillList
+	bool isAdding; ///< true when the list widget is adding items
+	bool allFinished; ///< true when all the bills are added
 };
 
 #endif // BILLLIST_H
